58ed367725ab599ee602ca962eaeb86b6d75daf6
Nirmal Fernando <nirmal070125@gmail.com>
2019-05-30 01:14:52 +0530

    update

--- client-samples/dotnet/dotnet-agent-oidc-sso/Sample/Site.Master.designer.cs
+++ client-samples/dotnet/dotnet-agent-oidc-sso/Sample/Site.Master.designer.cs
@@ -13,15 +13,6 @@ namespace Sample {
     public partial class SiteMaster {
         
         /// <summary>
-        /// userName control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlAnchor userName;
-        
-        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
